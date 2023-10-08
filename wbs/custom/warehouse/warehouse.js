e932d1055139d9e9f42c8a19096236f6bae718c6
Himanshu Shivhare <himanshushivhare047@gmail.com>
2023-10-08 17:27:49 +0530

    Update warehouse.js

--- wbs/custom/warehouse/warehouse.js
+++ wbs/custom/warehouse/warehouse.js
@@ -1,18 +1,3 @@
-// frappe.ui.form.on('Warehouse', {
-//   refresh: (frm) => {
-
-//     if (!frm.doc.__islocal) {
-//       frm.add_custom_button(__('Make WBS Warehouse & Specify WBS Settings'),() => {
-//         let local_docname = frappe.model.make_new_doc_and_get_name('WBS Settings');
-//         // let url = get_url()
-//         // console.log(get_url())
-//         frappe.set_route('wbs-settings/new-wbs-settings',{'warehouse': frm.doc.name});
-//       });
-
-//     }
-//   }
-// });
-
 frappe.ui.form.on('Warehouse', {
   refresh: (frm) => {
     if (!frm.doc.__islocal) {
